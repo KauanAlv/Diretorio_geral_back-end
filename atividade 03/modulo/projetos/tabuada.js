685ef14fdd8490fa5dd430ee3fc67c98ae2faf5b
KAUAN ALVES PEREIRA <25203696@CFP127ALUNO.LOCAL>
2026-03-02 16:22:04 -0300

    wip: criar funções para a tabuada

--- atividade 03/modulo/projetos/tabuada.js
+++ atividade 03/modulo/projetos/tabuada.js
@@ -4,3 +4,48 @@
  * Data: 25/02/2026 - (quarta-feira)
  * Versão 1.0
  *************************************************************************/
+
+const calculosMatematicos = require('../calculo.js')
+
+const validarEntradaTabuada = function (valorEntre) {
+    let entrada = Number(valorEntre)
+
+    if (entrada >= 2 && entrada <= 100) {
+        return true
+    } else {
+        return false
+    }
+}
+
+const validarContadorTabuada = function (valorAte) {
+    let contador = Number(valorAte)
+
+    if (contador >= 1 && contador <= 50) {
+        return true
+    } else {
+        return false
+    }
+}
+
+const calcularTabuada = function (valorInicial, valorFinal, contadorInicial, contadorFinal) {
+    let valorTabI = Number(valorInicial)
+    let valorTabF = Number(valorFinal)
+    let contI = Number(contadorInicial)
+    let contF = Number(contadorFinal)
+    let resultado = ''
+
+    for (let contador1 = valorTabI; contador1 <= valorTabF; contador1++) {
+        resultado += `\nTabuada do ${contador1}\n`
+
+        for (let valor1 = contI; valor1 <= contF; valor1++) {
+            resultado += `${contador1} x ${valor1} = ${calculosMatematicos.multiplicar(contador1, valor1)}\n`
+        }
+    }
+    return resultado
+}
+
+module.exports = {
+    validarEntradaTabuada,
+    validarContadorTabuada,
+    calcularTabuada
+}
