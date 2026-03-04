d45ca8caaa33eabb1ed521bc95c6e9718382efc4
KAUAN ALVES PEREIRA <25203696@CFP127ALUNO.LOCAL>
2026-03-04 14:01:33 -0300

    feat: criar funções pares, impares e suas validações

--- atividade 03/modulo/projetos/par_impar.js
+++ atividade 03/modulo/projetos/par_impar.js
@@ -4,3 +4,66 @@
  * Data: 25/02/2026 - (quarta-feira)
  * Versão 1.0
  **************************************************************************************/
+
+const validarParImparI = function (numeroParImparI) {
+    let numeroInicial = Number(numeroParImparI)
+
+    if (numeroInicial >= 0 && numeroInicial <= 500) {
+        return true
+    } else {
+        return false
+    }
+}
+
+const validarParImparF = function (numeroParImparF) {
+    let numeroInicial = Number(numeroParImparF)
+
+    if (numeroInicial >= 100 && numeroInicial <= 1000) {
+        return true
+    } else {
+        return false
+    }
+}
+
+const validarNumeroMaiorIgual = function (numeroInicial, numeroFinal) {
+    let numIni = Number(numeroInicial)
+    let numFim = Number(numeroFinal)
+
+    if (numIni >= numFim) {
+        return false
+    } else {
+        return true
+    }
+}
+
+const calcularNumPar = function (numeroInicio, numeroFim) {
+    let numI = Number(numeroInicio)
+    let numF = Number(numeroFim)
+    let resultado = ''
+    let contagem = 0
+
+    for (let numCont = numI; numCont <= numF; numCont++) {
+        if (numCont % 2 === 0) {
+            resultado += numCont + '\n'
+            contagem++
+        }
+    }
+
+    return resultado + '|' + contagem
+}
+
+const calcularNumImpar = function (numeroInicio, numeroFim) {
+    let numI = Number(numeroInicio)
+    let numF = Number(numeroFim)
+    let resultado = ''
+    let contagem = 0
+
+    for (let numCont = numI; numCont <= numF; numCont++) {
+        if (numCont % 2 !== 0) {
+            resultado += numCont + '\n'
+            contagem++
+        }
+    }
+
+    return resultado + '|' + contagem
+}
