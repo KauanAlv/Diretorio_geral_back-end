44edd849251a534258608bc18af4236dba17bc2f
KAUAN ALVES PEREIRA <25203696@CFP127ALUNO.LOCAL>
2026-03-18 16:37:38 -0300

    feat: adicionar função de sigla dos estados

--- atividade 04/modulo/funcoes.js
+++ atividade 04/modulo/funcoes.js
@@ -8,10 +8,25 @@
 let dados = require('./estados_cidades.js')
 
 const getListaDeEstados = function () {
+    let uf = []     //Cria um novo array
+    let quantidade  //Para contar a quantidade de estados
+    let listagem    //Para listar os estados com a quantidade
+
+    //Faz a busca de todos os estados na API
     dados.listaDeEstados.estados.forEach(function (itemEstado) {
 
+        //No novo array, adiciona a sigla de todos os estados já existentes
+        uf.push(itemEstado.sigla)
     })
 
+    //Com todos os estados no novo array, conta a quantidade
+    quantidade = uf.length
+
+    //Joga os estados e a quantidade dentro de um JSON,
+        //para os estados ficarem em um ARRAY e a quantidade fora desse ARRAY
+    listagem = { uf, quantidade }
+
+    return listagem
 }
 
 getListaDeEstados()
