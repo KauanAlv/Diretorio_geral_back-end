2e771c490095226051ea882665cb4136f938c319
KAUAN ALVES PEREIRA <25203696@CFP127ALUNO.LOCAL>
2026-03-18 16:16:46 -0300

    feat: adicionar atividade 04

---
+++ atividade 04/modulo/funcoes.js
@@ -0,0 +1,17 @@
+/*********************************************************************************************************************
+ * Objetivo: Arquivo responsável por conter todas as funções que faz a busca de Estados, Cidades e Capitais do Brasil
+ * Autor: Kauan
+ * Data: 18/03/2026 - (quarta-feira)
+ * Versão 1.0
+*********************************************************************************************************************/
+
+let dados = require('./estados_cidades.js')
+
+const getListaDeEstados = function () {
+    dados.listaDeEstados.estados.forEach(function (itemEstado) {
+
+    })
+
+}
+
+getListaDeEstados()
